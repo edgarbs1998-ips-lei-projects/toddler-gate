5818d27fd68a43e8402981c7df854af7850245ab
Henoch Vitureira <170221014@estudantes.ips.pt>
2019-11-22 20:34:58 +0000

    Android Project and gitignore

---
+++ app/src/main/java/com/example/toddlergate12/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.toddlergate12;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
