51060367b2324414bdc0987438b8374a69279abc
Henoch Vitureira <170221014@estudantes.ips.pt>
2019-11-23 00:58:28 +0000

    -Created Parents Area Actitivity -Added the buttons -Still missing the necessary spacing

---
+++ app/src/main/java/parents_area/Parents_Area_Main.java
@@ -0,0 +1,16 @@
+package parents_area;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.toddlergate12.R;
+
+public class Parents_Area_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parents_area_main);
+    }
+}
