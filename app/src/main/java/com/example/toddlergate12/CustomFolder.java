ef3a90a834b4896ec90bb2e0a90e5b2a568e307f
Henoch Vitureira <170221014@estudantes.ips.pt>
2019-12-08 23:46:54 +0000

    Custom folder activity has a preview list, working, with scroll and action bar padding

--- app/src/main/java/com/example/toddlergate12/CustomFolder.java
+++ app/src/main/java/com/example/toddlergate12/CustomFolder.java
@@ -3,21 +3,44 @@ package com.example.toddlergate12;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.File;
 
 public class CustomFolder extends AppCompatActivity {
 
-
+    ListView listview;
+    String[] dados;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_folder);
+        this.listview = (ListView) findViewById(R.id.file_listView);
 
+        Rect rectangle = new Rect();
+        Window window = getWindow();
+        window.getDecorView().getWindowVisibleDisplayFrame(rectangle);
+        int statusBarHeight = rectangle.top;
+        int contentViewTop =
+                window.findViewById(Window.ID_ANDROID_CONTENT).getTop();
+        int titleBarHeight= contentViewTop - statusBarHeight;
+
+        Log.d("*** Elenasys :: ", "StatusBar Height= " + statusBarHeight + " , TitleBar Height = " + titleBarHeight);
+
+        dados = new String[] { "Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread",
+                "Honeycomb", "Ice Cream Sandwich", "Jelly Bean",
+                "KitKat", "Lollipop", "Marshmallow", "Nougat" };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dados);
+
+        listview.setAdapter(adapter);
      /*   String path = Environment.getExternalStorageDirectory().toString()+"/Pictures";
         Log.d("Files", "Path: " + path);
         File directory = new File(path);
